7793f19e8ff0e5887fd51d3ad41d0cc9f5a86d76
ricardosilveira_ <72884776+Phyz4@users.noreply.github.com>
2022-03-23 16:42:46 +0000

    Delete fastapitest directory

--- fastapitest/models.py
+++
@@ -1,9 +0,0 @@
-"""from sqlalchemy import Column, Integer, String
-from database import Base
-# Base ou BaseModel ? 
-# Define To Do class inheriting from Base
-class ToDo(Base):
-    __tablename__ = 'todos'
-    id = Column(Integer, primary_key=True)
-    Task = Column(String(256))
-"""
